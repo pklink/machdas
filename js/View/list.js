77020deb101e79d9a681b2873f287359703f6b3c
Pierre Klink <dev@klinks.info>
2013-03-24 20:17:25 +0100

    added no-tasks-message

--- js/View/list.js
+++ js/View/list.js
@@ -4,23 +4,46 @@ $(function() {
 
         el: '#list',
 
+        isStarted: false,
 
         addTask: function(task) {
             var view = new Selleck.View.Task({model: task});
             this.$el.prepend(view.render().$el.hide());
-            view.$el.show();
+
+            if (this.isStarted) {
+                view.$el.slideDown();
+            }
+            else {
+                view.$el.show();
+            }
         },
 
 
         initialize:  function() {
             this.listenTo(Selleck.Collection.Tasks, 'add', this.addTask);
-
-            var view = this;
-            Selleck.Collection.Tasks.once('sync', function() {
-                view.$('.loading').hide();
-            });
+            this.listenTo(Selleck.Collection.Tasks, 'add', this.hideNoTasksMessage);
+            this.listenTo(Selleck.Collection.Tasks, 'remove', this.showNoTasksMessage);
+            this.listenTo(Selleck.Collection.Tasks, 'sync', this.showList);
 
             Selleck.Collection.Tasks.fetch();
+        },
+
+
+        hideNoTasksMessage: function() {
+            this.$('.no-tasks').slideUp();
+        },
+
+
+        showList: function() {
+            this.$el.slideDown();
+            this.isStarted = true;
+        },
+
+
+        showNoTasksMessage: function() {
+            if (Selleck.Collection.Tasks.length == 0) {
+                this.$('.no-tasks').slideDown();
+            }
         }
 
     });
