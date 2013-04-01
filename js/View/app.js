9e90ca427eef978c02e38d1a74ac4a8270c37cff
Pierre Klink <dev@klinks.info>
2013-04-01 15:12:57 +0200

    renamed Dingbat.View.App.form to Dingbat.View.App.Form

--- js/View/app.js
+++ js/View/app.js
@@ -16,7 +16,7 @@ $(function() {
         /**
          * @type Dingbat.View.Form
          */
-        form: null,
+        Form: null,
 
 
         /**
@@ -39,21 +39,21 @@ $(function() {
 
         initialize:  function() {
             // create views
-            this.form       = new Dingbat.View.Form();
+            this.Form       = new Dingbat.View.Form();
             this.list       = new Dingbat.View.List();
             this.Footer     = new Dingbat.View.Footer();
             this.Navigation = new Dingbat.View.Navigation();
             this.sidebar    = new Dingbat.View.Sidebar();
 
             // set views to layout/application
-            this.setView('.form', this.form);
+            this.setView('.form', this.Form);
             this.setView('.list', this.list);
             this.setView('footer', this.Footer);
             this.setView('.navigation', this.Navigation);
 
             // hide views
             this.Navigation.$el.hide();
-            this.form.$el.hide();
+            this.Form.$el.hide();
             this.list.$el.hide();
             this.Footer.$el.hide();
 
@@ -64,7 +64,7 @@ $(function() {
         showApp: function() {
             this.$('#app-loader').hide();
             this.Navigation.$el.fadeIn();
-            this.form.$el.slideDown();
+            this.Form.$el.slideDown();
             this.list.$el.fadeIn();
             this.Footer.$el.fadeIn();
             this.sidebar.render().$el.appendTo('body').hide().fadeIn();
