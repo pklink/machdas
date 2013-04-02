32faec8144ffe2bba6536a8803aaf3e8e3afc94f
Pierre Klink <dev@klinks.info>
2013-04-02 20:49:19 +0200

    execute shake-effect if update a task failed

--- js/View/task.js
+++ js/View/task.js
@@ -131,6 +131,8 @@ $(function() {
                 this.$('.name').text( this.model.get('name') );
                 this.$el.attr('class', 'prio-' + this.model.priorityName);
                 this.hideForm();
+            } else {
+                this.$('input.name').parent().shake();
             }
 
             return false;
