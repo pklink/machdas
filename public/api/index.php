91b5004c74475faf73f4c594977b683287ea1c96
Pierre Klink <dev@einself.net>
2016-03-29 00:44:56 +0200

    removed \Dingbat\App

--- public/api/index.php
+++ public/api/index.php
@@ -6,5 +6,41 @@ require __DIR__ . '/../../vendor/autoload.php';
 // load config
 $config = require __DIR__ . '/../../config.php';
 
-// create and run Dingbat
-(new \Dingbat\App($config))->run();
+// create app
+$app = new \Slim\App([
+    'settings' => [
+        'displayErrorDetails' => $config['debugging']
+    ]
+]);
+
+// prepare database
+$capsule = new \Illuminate\Database\Capsule\Manager();
+$capsule->addConnection([
+    'driver'    => 'mysql',
+    'host'      => $config['database']['host'],
+    'database'  => $config['database']['name'],
+    'username'  => $config['database']['username'],
+    'password'  => $config['database']['password'],
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => '',
+]);
+
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
+
+// routing
+$app->post('/cards', \Dingbat\Action\Card\Create::class . ':run');
+$app->get('/cards', \Dingbat\Action\Card\GetAll::class . ':run');
+$app->get('/cards/{slug}', \Dingbat\Action\Card\GetOne::class . ':run');
+$app->delete('/cards/{slug}', \Dingbat\Action\Card\Delete::class . ':run');
+$app->put('/cards/{slug}', \Dingbat\Action\Card\Update::class . ':run');
+
+$app->post('/tasks', \Dingbat\Action\Task\Create::class . ':run');
+$app->get('/tasks/{id}', \Dingbat\Action\Task\GetOne::class . ':run');
+$app->get('/tasks', \Dingbat\Action\Task\GetAll::class . ':run');
+$app->put('/tasks/{id}', \Dingbat\Action\Task\Update::class . ':run');
+$app->delete('/tasks/{id}', \Dingbat\Action\Task\Delete::class . ':run');
+
+// start app
+$app->run();
