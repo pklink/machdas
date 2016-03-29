113abac7a742afa9f93e281f45339bf0a5f0bbac
Pierre Klink <dev@einself.net>
2016-03-29 11:35:12 +0200

    added regex to route placeholders

--- public/api/index.php
+++ public/api/index.php
@@ -31,15 +31,15 @@ $capsule->bootEloquent();
 // routing
 $app->post('/cards', \Dingbat\Action\Card\Create::class . ':run');
 $app->get('/cards', \Dingbat\Action\Card\GetAll::class . ':run');
-$app->get('/cards/{id}', \Dingbat\Action\Card\GetOne::class . ':run');
-$app->delete('/cards/{id}', \Dingbat\Action\Card\Delete::class . ':run');
-$app->put('/cards/{id}', \Dingbat\Action\Card\Update::class . ':run');
+$app->get('/cards/{id:\d+}', \Dingbat\Action\Card\GetOne::class . ':run');
+$app->put('/cards/{id:\d+}', \Dingbat\Action\Card\Update::class . ':run');
+$app->delete('/cards/{id:\d+}', \Dingbat\Action\Card\Delete::class . ':run');
 
 $app->post('/tasks', \Dingbat\Action\Task\Create::class . ':run');
-$app->get('/tasks/{id}', \Dingbat\Action\Task\GetOne::class . ':run');
 $app->get('/tasks', \Dingbat\Action\Task\GetAll::class . ':run');
-$app->put('/tasks/{id}', \Dingbat\Action\Task\Update::class . ':run');
-$app->delete('/tasks/{id}', \Dingbat\Action\Task\Delete::class . ':run');
+$app->get('/tasks/{id:\d+}', \Dingbat\Action\Task\GetOne::class . ':run');
+$app->put('/tasks/{id:\d+}', \Dingbat\Action\Task\Update::class . ':run');
+$app->delete('/tasks/{id:\d+}', \Dingbat\Action\Task\Delete::class . ':run');
 
 // start app
 $app->run();
