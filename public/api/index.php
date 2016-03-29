e2efa36d92cd4b3d1dc7ce4b58d1b30cce1ab818
Pierre Klink <dev@einself.net>
2016-03-29 12:19:38 +0200

    improved coding style

--- public/api/index.php
+++ public/api/index.php
@@ -15,7 +15,7 @@ $container = new \Slim\Container([
 ]);
 
 // prepare eloquent
-$container['db'] = function ($container) {
+$container['db'] = function($container) {
     $capsule = new \Illuminate\Database\Capsule\Manager();
     $capsule->addConnection($container['settings']['db']);
     $capsule->setAsGlobal();
