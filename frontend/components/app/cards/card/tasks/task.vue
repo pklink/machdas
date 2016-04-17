f811d54419c68c01c0dbde28dc91e0de13948d38
Pierre Klink <dev@einself.net>
2016-04-17 23:35:54 +0200

    replaced delete modal by simple text confirmation

--- frontend/components/app/cards/card/tasks/task.vue
+++ frontend/components/app/cards/card/tasks/task.vue
@@ -1,42 +1,39 @@
 <template>
-    <a class="item">
-        <div class="right floated content" v-show="!editMode">
-            <i class="write icon" @click="edit"></i>
-            <i class="trash icon" @click="showWarning"></i>
+    <a class="item" v-show="!editMode && !deleteMode">
+        <div class="right floated content">
+            <i class="write icon" @click="toggleEditMode"></i>
+            <i class="trash icon" @click="toggleDeleteMode"></i>
         </div>
-        <div class="content" @click="toggle">
-            <div class="ui tiny red empty circular label" v-show="!editMode" v-if="model.priority > 500"></div>
-            <div class="ui tiny orange empty circular label" v-show="!editMode" v-if="model.priority == 500"></div>
-            <div class="ui tiny yellow empty circular label" v-show="!editMode" v-if="model.priority < 500">&nbsp</div>
-            <div class="ui small transparent fluid input" v-show="editMode">
-                <!--suppress HtmlFormInputWithoutLabel -->
-                <input type="text" v-model="model.name" @keyup.enter="save" @keyup.esc="cancel">
-            </div>
-            <span v-show="!model.isDone && !editMode">{{ model.name }}</span>
-            <del v-show="model.isDone && !editMode">{{ model.name }}</del>
-        </div>
-        <div class="ui small modal">
-            <div class="header">Delete Task</div>
-            <div class="content">
-                <p>Do you really want to delete this task?</p>
-            </div>
-            <div class="actions">
-                <div class="ui black deny button">
-                    Nope
-                </div>
-                <div class="ui red right labeled approve icon button" @click="delete">
-                    Yep, delete it
-                    <i class="trash icon"></i>
-                </div>
-            </div>
+        <div class="content" @click="toggleIsDone">
+            <div class="ui tiny red empty circular label" v-if="model.priority > 500"></div>
+            <div class="ui tiny orange empty circular label" v-if="model.priority == 500"></div>
+            <div class="ui tiny yellow empty circular label" v-if="model.priority < 500">&nbsp</div>
+            <span v-show="!model.isDone">{{ model.name }}</span>
+            <del v-show="model.isDone">{{ model.name }}</del>
         </div>
     </a>
+    <div class="item" v-show="editMode">
+        <div class="content">
+            <div class="ui small transparent fluid input" v-show="editMode">
+                <!--suppress HtmlFormInputWithoutLabel -->
+                <input type="text" v-model="model.name" @keyup.enter="save" @keyup.esc="toggleEditMode">
+            </div>
+        </div>
+    </div>
+    <div class="item" v-show="deleteMode">
+        <div class="right floated content">
+            <i class="remove icon" @click="toggleDeleteMode"></i>
+            <i class="checkmark icon" @click="delete"></i>
+        </div>
+        <div class="content">
+            Do you really want want to delete this task?
+        </div>
+    </div>
 </template>
 
 <script type="text/babel">
     import taskService from '../../../../../services/task'
     import Utils from '../../../../../services/utils'
-    import $ from 'jquery'
 
     // noinspection JSUnusedGlobalSymbols
     export default {
@@ -44,42 +41,30 @@
         props: ['model'],
 
         data() {
-            return { editMode: false }
-        },
-
-        ready() {
-            const modalEl = $(this.$el).find('.ui.modal')
-            this._modal = (action) => {
-                modalEl.modal(action)
+            return {
+                deleteMode: false,
+                editMode: false
             }
         },
 
         methods: {
-            toggle() {
-                this.model.isDone = !this.model.isDone
-                this.save()
-            },
             delete() {
                 taskService.delete(this.model)
             },
-            edit() {
-                this.editMode = true
-                this.$nextTick(() => {
-                    this.$el.getElementsByTagName('input')[0].focus()
-                })
-            },
-            cancel() {
-                this.editMode = false
-            },
             save() {
                 Utils.parseTask(this.model)
-
                 taskService.update(this.model.id, this.model)
             },
-            showWarning() {
-                this._modal('show')
+            toggleDeleteMode() {
+                this.deleteMode = !this.deleteMode
+            },
+            toggleEditMode() {
+                this.editMode = !this.editMode
+            },
+            toggleIsDone() {
+                this.model.isDone = !this.model.isDone
+                this.save()
             }
-
         }
 
     }
